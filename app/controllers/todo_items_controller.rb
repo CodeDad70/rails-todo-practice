c2f4c136f5004f17e1266bb0d0644fc0109d9a10
CodeDad <github email address>
2018-07-06 11:53:36 -0400

    created strong params for todo_items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -3,8 +3,7 @@ class TodoItemsController < ApplicationController
   before_action :set_todo_list
 
   def create 
-    
-    @todo_item = @todo_list.todo_items.create(params[:todo_item].permit)(:content)
+    @todo_item = @todo_list.todo_items.create(todo_items_params)
     redirect_to @todo_list
   end
 
@@ -13,5 +12,9 @@ class TodoItemsController < ApplicationController
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list.id])
   end
-  
+
+  def todo_items_params
+    params.require(:todo_item).permit(:content)
+  end
+
 end
