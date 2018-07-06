cd45c5061b033c449df0cbc21f4fbaa6e1e3a053
CodeDad <github email address>
2018-07-06 12:32:53 -0400

    created a destory method for todo_item

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -7,10 +7,20 @@ class TodoItemsController < ApplicationController
     redirect_to @todo_list
   end
 
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+      if @todo_item.destroy
+          flash[:success] = "Item Deleted"
+      else flash[:error] = "Can't Delete Item"
+      end
+      redirect_to @todo_list
+  end
+
+
   private
 
   def set_todo_list
-    @todo_list = TodoList.find(params[:todo_list.id])
+    @todo_list = TodoList.find(params[:todo_list_id])
   end
 
   def todo_items_params
